2f386ea7be520b870903a3058cc8c01777251f86
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-09-02 18:11:15 +0900

    Create Program.cs

---
+++ Flowing word/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace Flowing_word
+{
+    class Program
+    {
+        static string[] words;
+        static string isFlowing(string[] wd)
+        {
+            char last = char.Parse(wd[0].Substring(wd[0].Length-1));
+            char first= char.Parse(wd[1].Substring(0, 1));
+            int i = 1;  
+            do
+            {
+                if (!last.Equals(first)) {
+                    return "false";
+                } else
+                {
+                    if (i!=wd.Length-1)
+                    {
+                        last = char.Parse(wd[i].Substring(wd[i].Length - 1));
+                        first = char.Parse(wd[++i].Substring(0, 1));
+                    }
+                }
+
+            } while (i < wd.Length-1);
+
+            return("true");
+        }
+        static void Main(string[] args)
+        {
+            words = Console.ReadLine().ToLower().Split();
+            if (words.Length > 0) {
+                Console.WriteLine(isFlowing(words));
+            };
+
+        }
+    }
+}
