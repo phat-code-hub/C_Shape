8f4c263e5216ea65bc4dabb04d54169c300dafbf
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-09-07 18:46:20 +0900

    Create Prime String1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace Prime_String1
+{
+    class Program
+    {
+        static string word;
+        static string ans;
+        static String compareString()
+        {
+            
+            return "prime";
+        }
+        static void Main(string[] args)
+        {
+            word = Console.ReadLine().Trim().ToLower();
+            if (word.Length>0) {
+                ans=compareString();
+            }
+        }
+    }
+}
