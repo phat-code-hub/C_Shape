3b505eec4e35a3a896e229b04f1484f56350a127
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-09-13 18:05:19 +0900

    Update Program.cs

--- Halloween Candy/Program.cs
+++ Halloween Candy/Program.cs
@@ -10,9 +10,10 @@ namespace Halloween_Candy
             int houses = Convert.ToInt32(Console.ReadLine());
             if (houses >= 3)
             {
-                //Round the percentage of getting dollarBill
-                int getBillChange = (int)(2 * 100) / houses;
-                Console.WriteLine(getBillChange);
+                //Change  to double by set 2-> 2.0
+                double getBillPercentage = (2.0 * 100) / houses;
+                //Round the Percentage of getting dollarBill
+                Console.WriteLine(Math.Ceiling(getBillPercentage));
             }
             else Console.WriteLine("Visited Houses must be greater than 3!");
             
