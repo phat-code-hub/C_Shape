436b1f8f17a5629c0e9a24ef265e6fcf6bf499a9
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-09-13 17:37:28 +0900

    Halloween Candy

---
+++ Halloween Candy/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Halloween_Candy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //get visted houses numbers
+            int houses = Convert.ToInt32(Console.ReadLine());
+            if (houses >= 3)
+            {
+                //Round the percentage of getting dollarBill
+                int getBillChange = (int)(2 * 100) / houses;
+                Console.WriteLine(getBillChange);
+            }
+            else Console.WriteLine("Visited Houses must be greater than 3!");
+            
+        }
+    }
+}
