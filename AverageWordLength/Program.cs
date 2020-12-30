3928cf1093b57176d3386977075f380536ece7a2
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-12-30 13:31:41 +0900

    New Codes

---
+++ AverageWordLength/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AverageWordLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> lst = new List<string>();
+            lst.Add("Phat");
+            lst.Add("Tuan");
+            int sum = 0;
+            double tt = Math.Ceiling((26.0 / 5));
+            foreach( string s in lst)
+            {
+                Console.WriteLine (s);
+                sum += s.Length;
+            }
+            Console.WriteLine(tt);
+        }
+    }
+}
