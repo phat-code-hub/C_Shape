2adab490725438b3842933e02d754a7912031444
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2020-12-31 12:47:59 +0900

    New Codes

---
+++ The Spy Life/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace The_Spy_Life
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            string mess = Console.ReadLine().Trim();
+            string ans = "";
+            foreach (char c in mess)
+            {
+                if (Char.IsLetter(c))
+                {
+                    ans = c+ans;
+                };
+                
+            };
+            Console.WriteLine(ans);
+        }
+    }
+}
