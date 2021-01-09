eb4e95fa467687b4560c6864d73d13c51c0ac896
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2021-01-09 11:57:28 +0900

    PWord 2

---
+++ Password Validation 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Password_Validation_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum1 = 0, sum2 = 0;
+            string ans = "Weak";
+            string words = Console.ReadLine().Trim();
+            if (words.Length >=7)
+            {
+                foreach (char c in words)
+                {
+                    if (Char.IsDigit(c)) sum1++;
+                    if (Char.IsPunctuation(c)) sum2++;
+                }
+                ans = (sum1 >= 2 && sum2 >= 2) ? "Strong" : "Weak";
+
+            }
+            Console.WriteLine(ans);
+            
+        }
+    }
+}
