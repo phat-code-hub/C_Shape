ebe5f515eed005d7fd5f25a77f03acf260dc3df7
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2021-02-07 12:43:55 +0900

    Secrete Message

---
+++ Secrete Message/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Secrete_Message
+{
+    class Program
+    {
+        static char char_Invert(char c)
+        {
+            char w=' ';
+            if (Char.IsLetter(c))
+            {
+                w = (char)((int)'z' - ((int) c - (int) 'a'));
+            }
+            return w;
+        }
+        static void Main(string[] args)
+        {
+            string mess="";
+            char[] words = Console.ReadLine().Trim().ToLower().ToCharArray();
+            foreach (char letter in words) {
+                mess += char_Invert(letter);
+            }
+            Console.WriteLine(mess);
+
+        }
+    }
+}
