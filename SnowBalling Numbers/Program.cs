383f3141927eee39ad4c54405f714704f308e143
phat-code-hub <59404113+phat-code-hub@users.noreply.github.com>
2021-02-17 18:31:01 +0900

    SnowBalling Numbers

---
+++ SnowBalling Numbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SnowBalling_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n =Convert.ToInt32( Console.ReadLine());
+            int[] nums = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                nums[i] =Convert.ToInt32( Console.ReadLine());
+            }
+            string ans = "true";
+            int sum = 0;
+            for (int i=0;i<n; i++)
+            {
+                if (i==0)
+                {
+                    sum += nums[i];
+                } else
+                {
+                    if (nums[i]<= sum)
+                    {
+                        ans = "false";
+                        break;
+                    } else
+                    {
+                        sum += nums[i];
+                    }
+                }
+            }
+            Console.WriteLine(ans);
+
+        }
+    }
+}
